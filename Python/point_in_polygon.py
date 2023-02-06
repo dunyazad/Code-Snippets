b98d3d4ae981cf61958e30e2f03e9b7209ebf03d
Mickey <dunyazad@hanmail.net>
2023-02-07 01:51:26 +0900

    Create point_in_polygon.py

---
+++ Python/point_in_polygon.py
@@ -0,0 +1,18 @@
+def point_in_polygon(point, polygon):
+    x, y = point
+    inside = False
+    n = len(polygon)
+    p1x, p1y = polygon[0]
+    for i in range(1, n + 1):
+        if point[0] == p1x and point[1] == p1y:
+            return True
+        p2x, p2y = polygon[i % n]
+        if y > min(p1y, p2y):
+            if y <= max(p1y, p2y):
+                if x <= max(p1x, p2x):
+                    if p1y != p2y:
+                        xinters = (y - p1y) * (p2x - p1x) / (p2y - p1y) + p1x
+                    if p1x == p2x or x <= xinters:
+                        inside = not inside
+        p1x, p1y = p2x, p2y
+    return inside
