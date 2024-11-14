0b6b56e89548552bf58071e663c79490ce408a01
Mickey <dunyazad@hanmail.net>
2024-11-15 08:02:47 +0900

    Update Kernel.cu

--- CUDA/Kernel.cu
+++ CUDA/Kernel.cu
@@ -1,15 +1,15 @@
 __global__ Kernel_Sample(size_t numberOfThreads)
 {
-    size_t threadid = blockIdx.x * blockDim.x + threadIdx.x;
-		if (threadid > numberOfInputPoints - 1) return;  
+	size_t threadid = blockIdx.x * blockDim.x + threadIdx.x;
+	if (threadid > numberOfInputPoints - 1) return;  
 }
 
 void Calling(size_t numberOfThreads)
 {
-    int mingridsize;
-    int threadblocksize;
-    checkCudaErrors(cudaOccupancyMaxPotentialBlockSize(&mingridsize, &threadblocksize, Kernel_Sample, 0, 0));
-    auto gridsize = (numberOfThreads - 1) / threadblocksize;
+    	int mingridsize;
+    	int threadblocksize;
+    	checkCudaErrors(cudaOccupancyMaxPotentialBlockSize(&mingridsize, &threadblocksize, Kernel_Sample, 0, 0));
+    	auto gridsize = (numberOfThreads - 1) / threadblocksize;
     
-    Kernel_Sample << <gridsize, threadblocksize, 0, stream >> > ();
+	Kernel_Sample << <gridsize, threadblocksize, 0, stream >> > ();
 }
